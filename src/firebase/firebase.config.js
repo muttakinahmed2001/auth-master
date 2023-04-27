9cb4645419d6dcbf4caf9d311640629c76901e0d
unknown <muttakinahmed871@gmail.com>
2023-04-27 14:27:54 +0600

    implemented login and register

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzq7NjxWcWmmO2LiPo-8zoTj3dreE0Zf8",
+  authDomain: "auth-firebase-context-ta-fc33b.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-fc33b",
+  storageBucket: "auth-firebase-context-ta-fc33b.appspot.com",
+  messagingSenderId: "542821405220",
+  appId: "1:542821405220:web:ca6adb7f6d4eeb17f07eb1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
